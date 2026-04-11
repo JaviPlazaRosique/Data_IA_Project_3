de92df5430309685e8d762c98f0768220b200385
JaviPlazaRosique <j.plazarosique@gmail.com>
2026-04-11 20:39:01 +0200

    subir archivos web al bucket

--- terraform/modules/bucket_web/main.tf
+++ terraform/modules/bucket_web/main.tf
@@ -1,7 +1,8 @@
 resource "google_storage_bucket" "bucket_web" {
-  name          = var.nombre_bucket
-  location      = var.region
-  force_destroy = true
+  name                        = var.nombre_bucket
+  location                    = var.region
+  force_destroy               = true
+  uniform_bucket_level_access = true
   website {
     main_page_suffix = "index.html"
     not_found_page   = "index.html"
@@ -13,5 +14,20 @@ resource "google_storage_bucket_iam_binding" "web_publica" {
   role    = "roles/storage.objectViewer"
   members = [
     "allUsers"
+  ]
+}
+
+resource "null_resource" "compilar_subir_web" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      cd ${var.ruta_recurso_web}
+      npm install
+      npm run build
+      gcloud config set project ${var.id_proyecto}
+      gcloud storage rsync ./dist/ gs://${google_storage_bucket.bucket_web.name} --recursive
+    EOT
+  }
+  depends_on = [
+    google_storage_bucket_iam_binding.web_publica
   ]
 }
