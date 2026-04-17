22aafb7661203cf37c96a737ba0335dd2686c390
JaviPlazaRosique <j.plazarosique@gmail.com>
2026-04-17 12:39:47 +0200

    pipeline ingestion v1

---
+++ terraform/modules/bigquery/main.tf
@@ -0,0 +1,25 @@
+resource "google_bigquery_dataset" "dataset" {
+  project                    = var.id_proyecto
+  dataset_id                 = var.id_dataset
+  friendly_name              = var.nombre_dataset
+  location                   = var.ubicacion
+  delete_contents_on_destroy = var.eliminar_contenido_al_borrar
+}
+
+resource "google_bigquery_table" "tabla" {
+  for_each = { for tabla in var.tablas : tabla.id_tabla => tabla }
+
+  project             = var.id_proyecto
+  dataset_id          = google_bigquery_dataset.dataset.dataset_id
+  table_id            = each.value.id_tabla
+  schema              = each.value.schema_json
+  deletion_protection = var.proteccion_borrado
+
+  dynamic "time_partitioning" {
+    for_each = each.value.campo_particion != null ? [1] : []
+    content {
+      type  = "DAY"
+      field = each.value.campo_particion
+    }
+  }
+}
