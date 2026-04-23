7bb579ac4fc5261fe0eee8c42d052707731b9c17
cabesaEDEM <cabesa@edem.es>
2026-04-23 23:19:52 +0200

    mobile view

--- frontend/portal/src/pages/MapPage.tsx
+++ frontend/portal/src/pages/MapPage.tsx
@@ -764,7 +764,7 @@ export default function MapPage() {
     <div className="bg-surface text-on-surface h-screen overflow-hidden flex flex-col">
       <TopNav />
 
-      <main className="relative flex-1 flex flex-col">
+      <main className="relative flex-1 min-h-0 flex flex-col">
         {/* Map + Side Panel */}
         <section className="relative flex-1 min-h-0 overflow-hidden flex flex-col md:flex-row">
 
